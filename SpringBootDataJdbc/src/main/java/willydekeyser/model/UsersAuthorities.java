cfe2249eb38bede1294111965fd42e81a6ba5298
willydekeyser <willy.de.keyser@skynet.be>
2023-01-04 15:34:54 +0100

    Update UsersAuthorities

--- SpringBootDataJdbc/src/main/java/willydekeyser/model/UsersAuthorities.java
+++ SpringBootDataJdbc/src/main/java/willydekeyser/model/UsersAuthorities.java
@@ -1,11 +1,11 @@
 package willydekeyser.model;
 
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Table("users_authorities")
 public record UsersAuthorities(
-		@Column("authorities_id") Integer authorities,
-		@Column("users_id") Integer users) {
+		@Column("authorities_id") AggregateReference<Authorities, Integer> authorities) {
 
 }
